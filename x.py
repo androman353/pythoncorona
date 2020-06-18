bcd1618a62ad38f39657d01875017e73c297f36f
androman353 <67049718+androman353@users.noreply.github.com>
2020-06-18 12:26:27 +0400

    Add files via upload

---
+++ x.py
@@ -0,0 +1,29 @@
+from turtle import *
+i = 1
+color('blue', 'red',)
+begin_fill()
+i = 1
+
+while (i < 10):
+    forward(50)
+    right(50)
+    forward(50)
+    right(50)
+    forward(50)
+    left(34)
+    forward(23)
+    pos()
+    forward(50)
+    left(50)
+    backward(70)
+    lt(45)
+    forward(47)
+    lt(25)
+    fd(17)
+    fd(1)
+    rt(12)
+    fd(10)
+    i = i + 1
+end_fill()
+done()
+print ("oui oui c sa ouii !!")
